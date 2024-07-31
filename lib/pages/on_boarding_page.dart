3db9698046335ec8add8eaa1fffc738a6e3b5813
HappySolutions <heba.odesk.tasks@gmail.com>
2024-07-31 16:43:30 +0300

    add page view

--- lib/pages/on_boarding_page.dart
+++ lib/pages/on_boarding_page.dart
@@ -7,12 +7,33 @@ class OnBoardingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: OnBoardingItemWidget(
-            image: ImageUtility.badges,
-            title: 'Certification and Badges',
-            subTitle: 'Earn a certificate after completion of every course'),
+    final _pageController = PageController(
+      initialPage: 1,
+      viewportFraction: 1,
+    );
+
+    return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        scrollDirection: Axis.horizontal, // or Axis.vertical
+        children: const [
+          OnBoardingItemWidget(
+              image: ImageUtility.badges,
+              title: 'Certification and Badges',
+              subTitle: 'Earn a certificate after completion of every course'),
+          OnBoardingItemWidget(
+              image: ImageUtility.progressTracking,
+              title: 'Progress Tracking',
+              subTitle: 'Check your Progress of every course'),
+          OnBoardingItemWidget(
+              image: ImageUtility.offline,
+              title: 'Offline Access',
+              subTitle: 'Make your course available offline'),
+          OnBoardingItemWidget(
+              image: ImageUtility.courseCategory,
+              title: 'Course Catalog',
+              subTitle: 'View in which courses you are enrolled'),
+        ],
       ),
     );
   }
