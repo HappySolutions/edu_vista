eb0ed8d1660d1c07f0d2b28adc34d31e13bcb88c
HappySolutions <heba.odesk.tasks@gmail.com>
2024-08-18 11:39:47 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,41 @@
 import 'package:device_preview/device_preview.dart';
+import 'package:edu_vista/cubit/auth_cubit.dart';
+import 'package:edu_vista/firebase_options.dart';
 import 'package:edu_vista/pages/on_boarding_page.dart';
 import 'package:edu_vista/pages/splash_page.dart';
+import 'package:edu_vista/services/pref.service.dart';
 import 'package:edu_vista/utils/color.utility.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
-  runApp(DevicePreview(
-    enabled: !kReleaseMode,
-    builder: (context) {
-      return const MyApp();
-    },
+import 'dart:ui';
+
+import 'package:edu_vista/pages/home_page.dart';
+import 'package:edu_vista/pages/login_page.dart';
+import 'package:edu_vista/pages/reset_password_page.dart';
+import 'package:edu_vista/pages/signup_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await PreferencesService.init();
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  } catch (e) {
+    print('Failed to initialize Firebase: $e');
+  }
+
+  runApp(MultiBlocProvider(
+    providers: [BlocProvider(create: (ctx) => AuthCubit())],
+    child: DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) {
+        return const MyApp();
+      },
+    ),
   ));
 }
 
@@ -20,15 +45,44 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      scrollBehavior: _CustomScrollBehaviour(),
       debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
       theme: ThemeData(
-        fontFamily: 'PlusJakartaSans',
-        scaffoldBackgroundColor: ColorUtility.scaffoldBackground,
+        scaffoldBackgroundColor: ColorUtility.gbScaffold,
+        fontFamily: ' PlusJakartaSans',
         colorScheme: ColorScheme.fromSeed(seedColor: ColorUtility.main),
         useMaterial3: true,
       ),
-      home: const OnBoardingPage(),
+      onGenerateRoute: (settings) {
+        final String routeName = settings.name ?? '';
+        final Map? data = settings.arguments as Map?;
+        switch (routeName) {
+          case LoginPage.id:
+            return MaterialPageRoute(builder: (context) => LoginPage());
+          case SignUpPage.id:
+            return MaterialPageRoute(builder: (context) => SignUpPage());
+          case ResetPasswordPage.id:
+            return MaterialPageRoute(builder: (context) => ResetPasswordPage());
+          case OnBoardingPage.id:
+            return MaterialPageRoute(
+                builder: (context) => const OnBoardingPage());
+          case HomePage.id:
+            return MaterialPageRoute(builder: (context) => const HomePage());
+
+          default:
+            return MaterialPageRoute(builder: (context) => const SplashPage());
+        }
+      },
+      initialRoute: SplashPage.id,
     );
   }
+}
+
+class _CustomScrollBehaviour extends MaterialScrollBehavior {
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.mouse,
+        PointerDeviceKind.touch,
+      };
 }
--- lib/utils/color.utility.dart
+++ lib/utils/color.utility.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
 class ColorUtility {
-  static const scaffoldBackground = Color(0xfffcfcfc);
-  static const grey = Color(0xff858383);
-  static const secondry = Color(0xffF5BB06);
+  static const deepYellow = Color(0xffF5BB06);
+  static const gray = Color(0xff858383);
+  static const grayLight = Color(0xffD1D1D6);
+  static const grayExtraLight = Color(0xffEBEBEB);
   static const main = Color(0xff477B72);
-  static const mediumBlack = Color(0xff1D1B20);
+  static const mediumlBlack = Color(0xff1D1B20);
+  static const gbScaffold = Color(0xffFCFCFC);
 }
